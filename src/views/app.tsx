cc5621fa1d4ef3994f7daca06e4849a5cc5edf23
Uanderson Ricardo <uandersonrfs@gmail.com>
2026-03-13 00:08:44 -0300

    init project

---
+++ src/views/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
